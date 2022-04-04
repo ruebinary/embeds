f54cafbbc662c3ba83ba277b7782128c55793b91
Rue <kssfrigyes@gmail.com>
2022-04-04 15:23:55 +0200

    Revamp with breaking class system

--- index.js
+++ index.js
@@ -1,88 +1,90 @@
-function createEmbed( ) {
-    let embed = {
-        title: null,
-        description: null,
-        url: null,
-        color: null,
-        fields: [ ],
-        thumbnail: { url: null },
-        image: { url: null },
-        author: { name: null, url: null, icon_url: null },
-        footer: { text: null, icon_url: null },
+import fetch from 'node-fetch';
 
-        addTitle( str ) {
-            if ( !str || str.length > 256 ) throw new Error( "Title string must be between 1 and 256 characters" )
-            this.title = str
-            return embed;
-        },
-        addDescription( str ) {
-            if ( !str || str.length > 4096) throw new Error( "Description string must be between 1 and 4096 characters" )
-            this.description = str
-            return embed;
-        },
-        addUrl( url ) {
-            if ( !url || !isValidUrl( url ) ) throw new Error( "Must provide valid URL" )
-            this.url = url
-            return embed;
-        },
-        addColor( num ) {
-
-            return embed;
-        },
-        addThumbnail( url ) {
-            if ( !url || !isValidUrl( url ) ) throw new Error( "Must provide valid thumbnail URL" )
-            this.thumbnail.url = url
-            return embed;
-        },
-        addImage( url ) {
-            if ( !url || !isValidUrl( url ) ) throw new Error( "Must provide valid image URL" )
-            this.image.url = url
-            return embed;
-        },
-        addAuthor( name, url = null, icon_url = null ) {
-            if ( !name || name.length > 256 ) throw new Error( "Author name must be between 1 and 256 characters" )
-            if ( url && !isValidUrl( url ) ) throw new Error( "Must provide valid author URL" )
-            if ( icon_url && !isValidUrl( icon_url ) ) throw new Error( "Must provide valid author icon URL" )
-            this.author.name = name
-            this.author.url = url
-            this.author.icon_url = icon_url
-            return embed;
-        },
-        addFooter( text, icon_url = null ) {
-            if ( !text || text.length > 2048 ) throw new Error( "Footer text must be between 1 and 2048 characters" )
-            if ( icon_url && !isValidUrl( icon_url ) ) throw new Error( "Must provide valid footer icon URL" )
-            this.footer.text = text
-            this.footer.icon_url = icon_url
-            return embed;
-        },
-        addField( name, value, inline = false ) {
-            if ( !name || name.length > 256 ) throw new Error( "Field name must be between 1 and 256 characters" )
-            if ( !value || value.length > 1024) throw new Error( "Field value must be between 1 and 1024 characters" )
-            if ( inline && typeof inline !== "boolean") throw new Error( "Field inline argument must be a valid boolean" )
-            let newfield = { name: name, value: value, inline: inline }
-            this.fields.push( newfield )
-            return embed;
-        },
-        jsonify( ) {
-            return JSON.stringify( embed )
-        },
-        buildObject( ) {
-            let exportable = {
-                embeds: [ ]
-            }
-            exportable.embeds.push( JSON.parse(JSON.stringify(embed)) )
-            return exportable;
-        },
-        buildJson( ) {
-            let exportable = {
-                embeds: [ ]
-            }
-            exportable.embeds.push( embed )
-            return JSON.stringify( exportable )
-        }
+export class DiscordEmbed {
+    constructor () {
     }
-    return embed;
+
+    title = null;
+    description = null;
+    url = null;
+    color = null;
+    fields= [ ];
+    thumbnail = { url: null };
+    image = { url: null };
+    author = { name: null, url: null, icon_url: null };
+    footer = { text: null, icon_url: null };
+
+    addTitle( str ) {
+        if ( !str || str.length > 256 ) throw new Error( "Title string must be between 1 and 256 characters" )
+        this.title = str
+        return this;
+    };
+    addDescription( str ) {
+        if ( !str || str.length > 4096) throw new Error( "Description string must be between 1 and 4096 characters" )
+        this.description = str
+        return this;
+    };
+    addUrl( url ) {
+        if ( !url || !isValidUrl( url ) ) throw new Error( "Must provide valid URL" )
+        this.url = url
+        return this;
+    };
+    addColor( num ) {
+
+        return this;
+    };
+    addThumbnail( url ) {
+        if ( !url || !isValidUrl( url ) ) throw new Error( "Must provide valid thumbnail URL" )
+        this.thumbnail.url = url
+        return this;
+    };
+    addImage( url ) {
+        if ( !url || !isValidUrl( url ) ) throw new Error( "Must provide valid image URL" )
+        this.image.url = url
+        return this;
+    };
+    addAuthor( name, url = null, icon_url = null ) {
+        if ( !name || name.length > 256 ) throw new Error( "Author name must be between 1 and 256 characters" )
+        if ( url && !isValidUrl( url ) ) throw new Error( "Must provide valid author URL" )
+        if ( icon_url && !isValidUrl( icon_url ) ) throw new Error( "Must provide valid author icon URL" )
+        this.author.name = name
+        this.author.url = url
+        this.author.icon_url = icon_url
+        return this;
+    };
+    addFooter( text, icon_url = null ) {
+        if ( !text || text.length > 2048 ) throw new Error( "Footer text must be between 1 and 2048 characters" )
+        if ( icon_url && !isValidUrl( icon_url ) ) throw new Error( "Must provide valid footer icon URL" )
+        this.footer.text = text
+        this.footer.icon_url = icon_url
+        return this;
+    };
+    addField( name, value, inline = false ) {
+        if ( !name || name.length > 256 ) throw new Error( "Field name must be between 1 and 256 characters" )
+        if ( !value || value.length > 1024) throw new Error( "Field value must be between 1 and 1024 characters" )
+        if ( inline && typeof inline !== "boolean") throw new Error( "Field inline argument must be a valid boolean" )
+        let newfield = { name: name, value: value, inline: inline }
+        this.fields.push( newfield )
+        return this;
+    };
+    json( ) {
+        return JSON.stringify( this )
+    };
+    object( ) {
+        return JSON.parse(JSON.stringify( this ))
+    }
 }
+
+export function buildJSON( e ) {
+    let exports = { embeds: [ e ] }
+    return JSON.stringify( exports )
+}
+
+export function buildObject( e ) {
+    let exports = { embeds: [ e ] }
+    return JSON.parse( JSON.stringify( exports ) )
+}
+
 function isValidUrl( string ) {
     let url;
 
@@ -94,6 +96,5 @@ function isValidUrl( string ) {
 
     return url.protocol === "http:" || url.protocol === "https:";
 }
-module.exports = {
-    createEmbed,
-}
+
+export default DiscordEmbed
--- readme.md
+++ readme.md
@@ -1,78 +1,56 @@
 ## 📝 embeds
 >small JavaScript module that builds Discord embed objects
 
-embeds includes a validation layer for embed properties, so you never send malformed objects. also includes three functions to build your embed for production.
+embeds includes a validation layer for embed properties, so you never send malformed objects. also includes two functions to build your embed for production.
 
 ```js
-const { createEmbed } = require('@ruebinary/embeds')
+import DiscordEmbed from '@ruebinary/embeds'
 
-const myembed = createEmbed()
+const yourembed = new DiscordEmbed()
 .addTitle('Embed title')
 .addDescription('Embed description')
 
-console.log(myembed)
-
-//output will be {title: 'Embed title', description: 'Embed description'}
+console.log(yourembed)
 ```
 **Details**
-
-*Constructors will be added here soon*
 
 <details>
     <summary>
         <b>
             <code>
-                .buildObject()
+                buildObject(embed)
             </code>
         </b>
     </summary>
     Wraps the instance of the embed in a new object's "embeds" array, thus making it ready for POST.
 
-    const myembed = createEmbed()
+    const yourembed = new DiscordEmbed()
     .addTitle('Embed title')
-    .buildObject()
-    console.log(myembed)
-    
-    //output {embeds: [{title: 'Embed Title'}]}
+    .addDescription('Embed description')
+
+    console.log(buildObject(yourembed))
 </details>
 <details>
     <summary>
         <b>
             <code>
-                .buildJson()
+                buildJSON()
             </code>
         </b>
     </summary>
     Wraps the instance of the embed in a new object's "embeds" array, thus making it ready for POST; then stringifies so you can use it as JSON.
 
-    const myembed = createEmbed()
+    const yourembed = new DiscordEmbed()
     .addTitle('Embed title')
-    .buildJson()
-    console.log(myembed)
-    
-    //output {"embeds": [{"title": "Embed Title"}]}
-</details>
-<details>
-    <summary>
-        <b>
-            <code>
-                .jsonify()
-            </code>
-        </b>
-    </summary>
-    Stringifies your embed
+    .addDescription('Embed description')
 
-    const myembed = createEmbed()
-    .addTitle('Embed title')
-    .jsonify()
-    console.log(myembed)
-    
-    //output {"title": "Embed Title"}
+    console.log(buildJSON(yourembed))    
 </details>
 
 **Roadmap**
 
 - [ ] Finish readme
 
-copyright (c) ruebinary 2022 
+copyright (c) 2022 @ruebinary
+
 *embeds* is released under isc license
